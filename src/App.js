86a1f0580eba645a959734027a8b0065255348a2
Mert Efe <mertefeisawesome@gmail.com>
2026-03-09 14:09:54 +0300

    remove unused ref

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Nav from "./components/Nav";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-      <header>
-        <nav></nav>
-      </header>
-      <main></main>
-      <footer></footer>
+      <Nav />
+      <Header />
+      <Main />
+      <Footer />
     </>
   );
 }
