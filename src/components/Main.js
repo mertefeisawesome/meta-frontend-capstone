362ed167f0f83e59cbce97b7b627396008517d17
Mert Efe <mertefeisawesome@gmail.com>
2026-03-10 12:20:51 +0300

    main section

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,27 @@
 import React from "react";
 
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <>
+      <div className="specials">
+        <div className="specials-header">
+          <h2>Specials</h2>
+          <button>Online Menu</button>
+        </div>
+        <div className="specials-cards">
+          <div className="card">
+            
+          </div>
+        </div>
+      </div>
+      <div className="testimonials">
+        <h2>Testimonials</h2>
+      </div>
+      <div className="about">
+        <h2>About Us</h2>
+      </div>
+    </>
+  );
 };
 
 export default Main;
