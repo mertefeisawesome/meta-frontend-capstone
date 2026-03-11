95230d3cdb408a38cd79f1052e08d20994bbfe16
Mert Efe <mertefeisawesome@gmail.com>
2026-03-11 11:11:05 +0300

    header restructure for responsiveness

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,17 +3,19 @@ import React from "react";
 const Header = () => {
   return (
     <header>
-      <div className="header-left">
-        <h1>Little Lemon</h1>
-        <h4>Chicago</h4>
-        <p>
-          We are a family owned Mediterranean restaurant, focused on traditional
-          recipes served with a modern twist.
-        </p>
-        <button>Reserve a Table</button>
-      </div>
-      <div className="header-right">
-        <img src="./assets/images/restauranfood.jpg" alt="Restaurant Food" />
+      <div className="container">
+        <div className="header-left">
+          <h1>Little Lemon</h1>
+          <h4>Chicago</h4>
+          <p>
+            We are a family owned Mediterranean restaurant, focused on
+            traditional recipes served with a modern twist.
+          </p>
+          <button>Reserve a Table</button>
+        </div>
+        <div className="header-right">
+          <img src="./assets/images/restauranfood.jpg" alt="Restaurant Food" />
+        </div>
       </div>
     </header>
   );
